6bc3afeaa28805eae1ddfd0e6d43ec7c958057be
Martin Ott <pahaloom@gmail.com>
2015-10-21 04:33:35 +0300

    Added organization for team

--- src/main/java/com/sprhib/model/Team.java
+++ src/main/java/com/sprhib/model/Team.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +19,10 @@ public class Team {
 	private String name;
 	
 	private Integer rating;
+
+	@ManyToOne
+	@JoinColumn(name = "organization_id")
+	private Organization organization;
 	
 	public Integer getId() {
 		return id;
@@ -36,5 +42,10 @@ public class Team {
 	public void setRating(Integer rating) {
 		this.rating = rating;
 	}
-
+	public Organization getOrganization() {
+		return organization;
+	}
+	public void setOrganization(Organization organization) {
+		this.organization = organization;
+	}
 }
